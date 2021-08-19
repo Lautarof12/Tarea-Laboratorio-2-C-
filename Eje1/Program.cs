d2d2a83841642aa085255b4555121c89c5f81ae3
Lautarof12 <81997754+Lautarof12@users.noreply.github.com>
2021-08-19 17:03:40 -0300

    Add files via upload

---
+++ Eje1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Eje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numAleatorio;
+            Console.WriteLine("Programa para mostrar un numero al azar, desde el 1 hasta el 100.");
+            Random creador = new Random();
+            numAleatorio = creador.Next(1, 100);
+            Console.WriteLine("El numero generado aleatoriamente es: {0}", numAleatorio);
+            Console.ReadKey();
+
+        }
+    }
+}
